102f621d324f0986325949226d4a82112810a8db
dotkint <kentbandojo.delapena@gmail.com>
2025-08-10 22:54:10 +0800

    modify image path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,11 +23,13 @@ const Hero = () => {
             <div>
                 <div className="flex items-center gap-3">
                     <figure className="img-box w-9 h-9 rounded-lg">
-                        <img src="/images/photo.png" 
-                            width={40}
-                            height={40}
-                            alt="Kent Dela Pena Photo"
-                            className="img-cover" />
+                        <img
+                        src={`${import.meta.env.BASE_URL}images/photo.png`}
+                        width={40}
+                        height={40}
+                        alt="Kent Dela Peña Photo"
+                        className="img-cover"
+                        />
                     </figure>
 
                     <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
@@ -63,11 +65,13 @@ const Hero = () => {
 
             <div className="hidden lg:block">
                 <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to 65% rounded-[60px] overflow-hidden">
-                    <img src="/images/photo.png" 
+                    <img
+                        src={`${import.meta.env.BASE_URL}images/photo.png`}
                         width={656}
                         height={800}
-                        alt="Kent Dela Pena Image"
-                        className="w-full" />
+                        alt="Kent Dela Peña Image"
+                        className="w-full"
+                        />
                 </figure>
             </div>
 
